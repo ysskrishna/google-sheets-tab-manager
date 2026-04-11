71688d400c9f4fdef8f2339b21d942e5bfa23345
ysskrishna <sivasaikrishnassk@gmail.com>
2026-04-11 16:09:53 +0530

    Siva | cleanup layout sizes

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://ysskrishna.github.io',
-   base: '/google-sheets-tab-manager/',
   trailingSlash: 'never',
   build: {
     format: 'file',
