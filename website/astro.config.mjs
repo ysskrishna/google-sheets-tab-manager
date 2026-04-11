818e06b4b38d27809d080d448d04c51fb49aaa08
ysskrishna <sivasaikrishnassk@gmail.com>
2026-04-11 17:10:33 +0530

    Siva | Minor bugfix with config

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   site: 'https://ysskrishna.github.io',
-  trailingSlash: 'never',
+  base: isProd ? '/google-sheets-tab-manager/' : '/',
   build: {
     format: 'file',
   },
